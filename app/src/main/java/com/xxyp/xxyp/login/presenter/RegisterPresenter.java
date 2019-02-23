1729f055dc927ef8923677a3182ad87e5cf72afe
Mango <sunpengfei0012@sina.cn>
2019-02-23 12:43:56 +0800

    [孙鹏飞]-[develop_spf]-[修改语音录制  增加发送约拍]

--- app/src/main/java/com/xxyp/xxyp/login/presenter/RegisterPresenter.java
+++ app/src/main/java/com/xxyp/xxyp/login/presenter/RegisterPresenter.java
@@ -7,18 +7,27 @@ import android.text.TextUtils;
 
 import com.xxyp.xxyp.R;
 import com.xxyp.xxyp.common.bean.UserInfo;
+import com.xxyp.xxyp.common.log.XXLog;
 import com.xxyp.xxyp.common.utils.ToastUtil;
+import com.xxyp.xxyp.common.utils.compress.LuBan;
 import com.xxyp.xxyp.common.utils.gallery.GalleryActivity;
 import com.xxyp.xxyp.common.utils.gallery.GalleryProvider;
+import com.xxyp.xxyp.common.utils.qiniu.QiNiuManager;
+import com.xxyp.xxyp.common.utils.qiniu.QiNiuUploadCallback;
 import com.xxyp.xxyp.login.contract.RegisterContract;
 import com.xxyp.xxyp.login.model.RegisterModel;
-import com.xxyp.xxyp.login.utils.UserConfig;
+import com.xxyp.xxyp.user.provider.UserProvider;
 
+import java.io.File;
 import java.util.List;
 
+import rx.Emitter;
+import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
@@ -101,7 +110,94 @@ public class RegisterPresenter implements RegisterContract.Presenter {
         if (paths != null && paths.size() > 0) {
             mPhoto = paths.get(0);
             mView.showAvatar("file://" + mPhoto);
+            mView.showRegisterLoading(true);
+            Subscription subscription = Observable.just(mPhoto)
+                    .flatMap(new Func1<String, Observable<?>>() {
+                        @Override
+                        public Observable<?> call(String s) {
+                            if (TextUtils.isEmpty(s)) {
+                                // 如果没有图片 则直接更新
+                                return Observable.just(null);
+                            }
+                            return uploadAvatar(mPhoto);
+                        }
+                    }).subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Subscriber<Object>() {
+                        @Override
+                        public void onCompleted() {
+
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            if (mView != null) {
+                                mView.cancelRegisterLoading();
+                            }
+                            XXLog.log_e("RegisterPresenter",
+                                    "updateUserInfo is failed" + e.getMessage());
+                        }
+
+                        @Override
+                        public void onNext(Object o) {
+                            if (mView != null) {
+                                mView.cancelRegisterLoading();
+                            }
+                        }
+                    });
+            mSubscription.add(subscription);
         }
+    }
+
+    /**
+     * 上传头像
+     *
+     * @return Observable
+     */
+    private Observable<Object> uploadAvatar(String pic) {
+        if (TextUtils.isEmpty(pic)) {
+            return Observable.just(null);
+        }
+        File file = new File(pic);
+        return LuBan.getInstance().compress(file).putGear(LuBan.THIRD_GEAR).asObservable()
+                .subscribeOn(Schedulers.computation()).map(new Func1<File, String>() {
+                    @Override
+                    public String call(File file) {
+                        return file.getAbsolutePath();
+                    }
+                }).flatMap(new Func1<String, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(final String s) {
+                        return Observable.create(new Action1<Emitter<String>>() {
+                            @Override
+                            public void call(final Emitter<String> pairEmitter) {
+                                QiNiuManager.getInstance().uploadImage(s, new QiNiuUploadCallback() {
+                                    @Override
+                                    public void onProgress(int progress) {
+
+                                    }
+
+                                    @Override
+                                    public void onSuccess(String file) {
+                                        pairEmitter.onNext(file);
+                                    }
+
+                                    @Override
+                                    public void onError(int errorCode, String msg) {
+                                        ToastUtil.showTextViewPrompt("上传头像失败");
+                                        XXLog.log_e("PersonalInfoPresenter", "uploadImage is failed" + msg);
+                                        pairEmitter.onError(new Throwable(msg));
+                                    }
+                                });
+                            }
+                        }, Emitter.BackpressureMode.BUFFER).observeOn(Schedulers.computation());
+                    }
+                }).flatMap(new Func1<String, Observable<Object>>() {
+                    @Override
+                    public Observable<Object> call(String s) {
+                        mUserInfo.setUserImage(s);
+                        return UserProvider.updateUserInfo(mUserInfo);
+                    }
+                });
     }
 
     @Override
